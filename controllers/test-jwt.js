f624e1ba47397add499d0fc82c87270411937ed9
LockVision <hassanelmaayati@gmail.com>
2026-09-08 11:02:27 +0300

    .

--- controllers/test-jwt.js
+++ controllers/test-jwt.js
@@ -1,20 +1,17 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
-const signToken = (req, res) => { const user = {
+const signToken = (req, res) => {
+  const user = {
     _id: 1,
-    username: 'test',
-    password: 'test',}
-  
-  
-  
-    const token = jwt.sign(user, process.env.JWT_SECRET);
+    username: "test",
+    password: "test",
+  };
 
+  const token = jwt.sign(user, process.env.JWT_SECRET);
+  res.json({ message: "You are Auth'd", token });
+};
 
-
-  }
-
-
-const verifyToken = (req, res) => {}
+const verifyToken = (req, res) => {};
 module.exports = {
   signToken,
   verifyToken,
