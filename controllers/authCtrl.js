93d343d8280d8d8d720a44d099ee1aa98004c22c
LockVision <hassanelmaayati@gmail.com>
2026-09-08 13:19:07 +0300

    .

--- controllers/authCtrl.js
+++ controllers/authCtrl.js
@@ -4,7 +4,15 @@ const User = require('../models/user');
 
 const SALT_ROUDS = 10;
 
-const signup = async (req, res) => {}
+const signup = async (req, res) => {
+try{}catch(err){
+
+  console.log(err)
+  res.status(500).json({err: 'something went wrong'})
+}
+
+
+}
 
 module.exports = {
   signup,
