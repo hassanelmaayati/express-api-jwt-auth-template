4c61b3f1365d060a981c1b704ff94b1a8a13a335
LockVision <hassanelmaayati@gmail.com>
2026-09-08 13:20:20 +0300

    .

--- controllers/authCtrl.js
+++ controllers/authCtrl.js
@@ -1,19 +1,40 @@
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const User = require('../models/user');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
 
 const SALT_ROUDS = 10;
 
 const signup = async (req, res) => {
-try{}catch(err){
+  try {
+    // verify if the username alrady exists
+    const userInDatabase = await User.findOne({ username: req.body.username });
+    // if the user exists send error msg
+    if (userInDatabase) {
+      return res.status(409).json({ err: "Invalid input" });
+    }
 
-  console.log(err)
-  res.status(500).json({err: 'something went wrong'})
-}
+    // Encrypt the password
+    const hashedPassword = bcrypt.hashSync(req.body.password, SALT_ROUDS);
+    req.body.password = hashedPassword;
 
+    // else lets check if the password match
+    // if password matches create the new user
+    const user = await User.create(req.body);
+    const payload = {
+      username: user.username,
+      _id: user._id,
+    };
 
-}
+    console.log(process.env.JWT_SECRET);
+    const token = jwt.sign(payload, process.env.JWT_SECRET);
+
+    res.status(201).json({ user, token });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ err: "something went wrong" });
+  }
+};
 
 module.exports = {
   signup,
-}
+};
