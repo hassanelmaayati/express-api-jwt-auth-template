2d7406ae7b831e1460b8720bc41f946621e33615
LockVision <hassanelmaayati@gmail.com>
2026-09-08 10:57:32 +0300

    .

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const logger = require('morgan');
 
+
+const testJWTCtrl=require('./controllers/test-jwt')
 mongoose.connect(process.env.MONGODB_URI);
 
 mongoose.connection.on('connected', () => {
