3b17a6b40a41e4000135fb286b62d3d180d2308e
LockVision <hassanelmaayati@gmail.com>
2026-09-08 11:02:54 +0300

    .

--- controllers/test-jwt.js
+++ controllers/test-jwt.js
@@ -11,7 +11,13 @@ const signToken = (req, res) => {
   res.json({ message: "You are Auth'd", token });
 };
 
-const verifyToken = (req, res) => {};
+const verifyToken = (req, res) => {const token = req.headers.authorization.split(' ')[1];
+
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  res.json({ message: 'Token is valid', token: decoded });
+};
+
+
 module.exports = {
   signToken,
   verifyToken,
